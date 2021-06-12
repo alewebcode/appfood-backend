2564bac2e5708219949779646c5684d403110037
alexandre <alexandre@webcodejf.com.br>
2021-06-11 23:26:24 -0300

    alteracao porta heroku

--- src/server.ts
+++ src/server.ts
@@ -12,5 +12,6 @@ app.use(cors());
 app.use(express.json())
 app.use(routes)
 app.use('/uploads', express.static(path.join(__dirname,'..','uploads')))
-app.listen(3333)
+
+app.listen(process.env.PORT || 3333)
 
